d77ef5659e71bbf417ae0d5097918faee141ab97
Adam Ochonicki <adam@fromonesrc.com>
2015-11-06 14:58:16 -0500

    Track and mic input for liiiiines

--- sketch.js
+++ sketch.js
@@ -2,13 +2,33 @@ var spectrum, amplitude, level, cnv, seed;
 var r, g, b;
 var soundFile, analyzer, delay;
 var colors;
+var tracks = [
+    'beatz.mp3',
+    'demo-riff.mp3'
+]
+
+function preload() {
+  track = tracks[floor(random(0, tracks.length))];
+  loadTrack = loadSound(track);
+}
 
 function setup() {
   cnv = createCanvas(windowWidth, windowHeight);
 
+  mic = new p5.AudioIn();
+  mic.start();
+  mic.amp(10);
 
   analyzer = new p5.Amplitude();
+  //analyzer.setInput(loadTrack);
   analyzer.setInput(mic);
+  
+  fft = new p5.FFT();
+  //fft.setInput(loadTrack);
+  fft.setInput(mic);
+  
+  //loadTrack.loop();
+  //loadTrack.amp(20);
   
   colors = new Array();
   for (var i=0; i<5; i++)
@@ -21,11 +41,11 @@ function generateColor() {
     return '#' + Math.floor((Math.random() * 0xF00000) + 0x0FFFFF).toString(16);
 }
 
-// random color bg expanding ellipse with volume
 function draw() {
+  var volume = analyzer.getLevel();
   for (var i=0; i<colors.length; i++) {
     fill(colors[i]);
-    rect(0,(height/5)*i,width,height/5);
+    rect(0,(height/5)*i,width,volume*100);
   }
 }
 
