032b085ed37f86c04f50d367d019b90399405f91
Adam Ochonicki <adam@fromonesrc.com>
2015-11-08 12:01:27 -0500

    Fuck yeah

--- sketch.js
+++ sketch.js
@@ -1,55 +1,94 @@
-var spectrum, amplitude, level, cnv, seed;
-var r, g, b;
-var soundFile, analyzer, delay;
-var colors;
-var tracks = [
-    'beatz.mp3',
-    'demo-riff.mp3'
-]
+var _p5, sound;
+var client_id = '008bd86a04b2625defc34d368fb72d2d';
+var url = 'https://soundcloud.com/brainfeeder/thundercat-oh-sheit-its-x';
+//var transforms, randTrans;
 
-function preload() {
-  track = tracks[floor(random(0, tracks.length))];
-  loadTrack = loadSound(track);
-}
+var sketch = function(s) {
+  var ticks = [];
+  var canvas;
+  var freq;
 
-function setup() {
-  cnv = createCanvas(windowWidth, windowHeight);
-
-  mic = new p5.AudioIn();
-  mic.start();
-  mic.amp(10);
-
-  analyzer = new p5.Amplitude();
-  //analyzer.setInput(loadTrack);
-  analyzer.setInput(mic);
-  
-  fft = new p5.FFT();
-  //fft.setInput(loadTrack);
-  fft.setInput(mic);
-  
-  //loadTrack.loop();
-  //loadTrack.amp(20);
-  
-  colors = new Array();
-  for (var i=0; i<5; i++)
-  {
-      colors.push(generateColor());
+  s.preload = function(){
+    s.background(0);
+    track = s.loadSound(sound.stream_url + '?client_id=' + client_id);
   }
-}
 
-function generateColor() {
-    return '#' + Math.floor((Math.random() * 0xF00000) + 0x0FFFFF).toString(16);
-}
+  s.setup = function() {
+    canvas = s.createCanvas(s.windowWidth, s.windowHeight);
 
-function draw() {
-  var volume = analyzer.getLevel();
-  for (var i=0; i<colors.length; i++) {
-    fill(colors[i]);
-    rect(0,(height/5)*i,width,volume*100);
+    amp = new p5.Amplitude(.50);
+    fft = new p5.FFT(.9, 256);
+    freq = fft.analyze();
+    for (var i = 0; i < freq.length; i++){
+      ticks.push(0);
+    }
+
+    track.loop();
+    s.background(0);
+
+
+    // transforms = [
+    //   function(x,y) { s.radians(x,y) },
+    //   function(x,y) { s.sin(x,y) },
+    //   function(x,y) { s.acos(x,y) }
+    // ]
+
+    // randTrans = s.floor(s.random(0,transforms.length-1));
   }
-}
+  
+  s.draw = function() {
+    var level = amp.getLevel();
+    freq = fft.analyze();
 
-function shuffle(o){
-  for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-  return o;
-}
+    s.background(0, 25);
+    s.textSize(20);
+    s.text(sound.title, 20, s.height - 20);
+    s.translate(s.width / 2, s.height / 2);
+
+    var freqLoopNum = freq.length - 90;
+    //s.transform(0,0);
+    for (var i = 0; i < freqLoopNum; i++){
+      ticks[i] += freq[i] / 2000;
+      var offset = 10 + i * 3;
+      var xPos = offset * s.cos(s.TWO_PI + ticks[i]);
+      var yPos = offset * s.sin(s.TWO_PI + ticks[i]);
+
+      s.push();
+
+      var a1 = s.map(level, 0, 1, 0, 255);
+      var a2 = s.map(freqLoopNum, 0, 1024, 0, 255);
+      var c1 = s.color(32, 219, 231, a1);
+      var c2 = s.color(74, 17, 89, a2);
+      var l = s.lerpColor(c1, c2, i / freqLoopNum);
+
+      s.fill(l);
+      s.noStroke();
+      s.translate(xPos, yPos);
+      //s.transform(xPos,yPos);
+      s.rotate(s.radians(xPos,yPos));
+      var h = 1 + i * 4 + freq[i] / 20;
+      s.rect(0, 0, freq[i] / 40, h);
+      s.pop();
+    }
+  }
+
+  // s.transform = function(x,y) {
+  //   return transforms[randTrans].call(x,y);
+  // }
+  
+  s.windowResized = function() {
+    s.background(0);
+    canvas.resize(s.windowWidth, s.windowHeight);
+  }
+  
+  s.mousePressed = function() {
+    track.isPlaying() ? track.pause() : track.play();
+  }
+};
+
+SC.initialize({ client_id: client_id });
+
+SC.get('/resolve', { url: url }, function(data){
+    sound = data;
+    _p5 = new p5(sketch);
+});
